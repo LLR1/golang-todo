719792b45bcf9cce97bd1ab838bd63f7c079728a
LLR1 <alexmadov27@gmail.com>
2025-03-20 16:44:12 +0100

    Allow tasks to contain multiple words using strings.Join

--- golang-todo.go
+++ golang-todo.go
@@ -52,7 +52,8 @@ func main() {
 			fmt.Println("Please specify a task to add.")
 			return
 		}
-		task := os.Args[2]
+		// Join all arguments after "add" as one string
+		task := strings.Join(os.Args[2:], " ")
 		tasks = append(tasks, task)
 		if err := saveTasks(); err != nil {
 			fmt.Println("Error saving tasks:", err)
@@ -60,29 +61,24 @@ func main() {
 		}
 		fmt.Println("Task added:", task)
 	case "list":
-		// If no tasks, inform the user
 		if len(tasks) == 0 {
 			fmt.Println("Task list is empty.")
 			return
 		}
-		// Print all tasks
 		fmt.Println("Task list:")
 		for i, t := range tasks {
 			fmt.Printf("%d. %s\n", i+1, t)
 		}
 	case "delete":
-		// Check if a task number is provided
 		if len(os.Args) < 3 {
 			fmt.Println("Please specify the task number to delete.")
 			return
 		}
-		// Convert the task number from string to integer
 		index, err := strconv.Atoi(os.Args[2])
 		if err != nil || index < 1 || index > len(tasks) {
 			fmt.Println("Invalid task number.")
 			return
 		}
-		// Remove the task from the slice
 		removed := tasks[index-1]
 		tasks = append(tasks[:index-1], tasks[index:]...)
 		if err := saveTasks(); err != nil {
@@ -95,7 +91,6 @@ func main() {
 	}
 }
 
-// printUsage displays the usage instructions for the CLI tool
 func printUsage() {
 	fmt.Println("Usage:")
 	fmt.Println("  add <task>    - add a new task")
